479d68bcf3f59ba03303789777c8f44024b814bd
Yavnesh Sharma <yavnesh.sharma@gmail.com>
2024-01-09 21:10:49 +0530

    mobile-sidebar implemented

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,55 +4,56 @@ import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const routes = [
     {
         label: "Dashboard",
         icon: LayoutDashboard,
         href: "/dashboard",
-        color: "text-white"
+        color: "text-sky-500"
     },
     {
         label: "Chat",
         icon: MessageSquare,
         href: "/chat",
-        color: "text-white"
+        color: "text-violet-500"
     },
     {
         label: "Image",
         icon: ImageIcon,
         href: "/image",
-        color: "text-white"
+        color: "text-pink-700"
     },
     {
         label: "Video",
         icon: VideoIcon,
         href: "/video",
-        color: "text-white"
+        color: "text-orange-700"
     },
     {
         label: "Music",
         icon: Music,
         href: "/music",
-        color: "text-white"
+        color: "text-emerald-500"
     },
     {
         label: "Coding",
         icon: Code,
         href: "/code",
-        color: "text-white"
+        color: "text-green-700"
     },
     {
         label: "Settings",
         icon: Settings,
         href: "/settings",
-        color: "text-white"
     },
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname();
     return(
-        <div className="space-y-4 py-4 flex flex-col h-full bg-slate-500 text-white">
+        <div className="space-y-4 py-4 flex flex-col h-full bg-slate-900 text-white">
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
                     <div className="relative w-8 h-8 mr-4">
@@ -66,7 +67,7 @@ const Sidebar = () => {
                     {routes.map((route) => (
                         <Link href={route.href}
                         key={route.href}
-                        className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-black hover:bg-white rounded-lg transition">
+                        className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-500")}>
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
                                 {route.label}
